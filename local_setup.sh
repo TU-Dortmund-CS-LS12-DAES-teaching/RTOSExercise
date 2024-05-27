809bed7597950d3a6aa28dae2d2f0048b1155517
kb1t <kb1t@users.noreply.github.com>
2024-05-27 12:30:44 +0200

    updated dependencies

--- local_setup.sh
+++ local_setup.sh
@@ -8,19 +8,19 @@ git clone --depth 1 --branch v5.1.4 --recursive https://github.com/espressif/esp
 
 mkdir watchy
 cd watchy
-git clone --depth 1 --branch 1.14.2 https://github.com/adafruit/Adafruit_BusIO.git
-git clone --depth 1 --branch 1.11.7 https://github.com/adafruit/Adafruit-GFX-Library.git
+git clone --depth 1 --branch 1.16.1 https://github.com/adafruit/Adafruit_BusIO.git
+git clone --depth 1 --branch 1.11.9 https://github.com/adafruit/Adafruit-GFX-Library.git
 git clone --depth 1 --branch 3.0.0-rc3 https://github.com/espressif/arduino-esp32.git arduino && \
     cd arduino && \
     git submodule update --init --recursive && cd ..
 git clone --depth 1 --branch 0.2.0 https://github.com/arduino-libraries/Arduino_JSON.git
 git clone --depth 1 --branch 2.0.1 https://github.com/JChristensen/DS3232RTC.git
-git clone --depth 1 --branch 1.5.2 https://github.com/ZinggJM/GxEPD2.git
+git clone --depth 1 --branch 1.5.6 https://github.com/ZinggJM/GxEPD2.git
 git clone --depth 1 --branch 3.2.1 https://github.com/arduino-libraries/NTPClient.git
 git clone --depth 1 --branch 1.0.3 https://github.com/orbitalair/Rtc_Pcf8563.git
 git clone --depth 1 --branch v1.6.1 https://github.com/PaulStoffregen/Time.git
 git clone --depth 1 --branch v1.4.7 https://github.com/sqfmi/Watchy.git
-git clone --depth 1 --branch v2.0.16-rc.2 https://github.com/tzapu/WiFiManager.git
+git clone --depth 1 --branch v2.0.17 https://github.com/tzapu/WiFiManager.git
 
 cd $thispath
 cp patches/Arduino_JSON.cmake esp/watchy/Arduino_JSON/CMakeLists.txt
